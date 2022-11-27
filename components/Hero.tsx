1015d385fe960ff1fcc71a295770db98cf06af49
aldrinn <48447138+aldringordon@users.noreply.github.com>
2022-11-27 17:19:53 +0800

    feat: add image to hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -17,6 +17,11 @@ function Hero({}: Props) {
   return (
     <div className="flex flex-col items-center justify-center h-screen space-y-8 overflow-hidden text-center">
       <BackgroundCircles />
+      <img
+        className="relative object-cover w-32 h-32 mx-auto rounded-full"
+        src="https://aldringordon.com/assets/Aldrin.jpg"
+        alt=""
+      />
       <h1>
         <span>{text}</span>
         <Cursor cursorColor="#F8FFAE" />
