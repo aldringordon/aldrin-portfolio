c78b2268ee4a2656a2a11e9c0bb87be0c4c3794f
aldrinn <48447138+aldringordon@users.noreply.github.com>
2022-11-27 21:20:49 +0800

    refactor: change from motionimg to img

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import { motion } from "framer-motion";
 import React from "react";
 
@@ -16,9 +17,10 @@ function Skill({ directionLeft }: Props) {
       whileInView={{ opacity: 1, x: 0 }}
       className="relative flex cursor-pointer group"
     >
-      <motion.img
+      <img
         src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png"
         className="object-cover w-24 h-24 transition duration-300 ease-in-out border border-gray-500 rounded-full md:w-28 md:h-28 xl:w-32 xl:h-32 filter group-hover:grayscale"
+        alt=""
       />
       <div className="absolute z-0 w-24 h-24 transition duration-300 ease-in-out rounded-full opacity-0 group-hover:opacity-80 group-hover:bg-white md:w-28 md:h-28 xl:w-32 xl:h-32">
         <div className="flex items-center justify-center h-full">
