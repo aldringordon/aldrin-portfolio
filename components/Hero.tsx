a5933e1085c2dfbe6636efc5ac3b9acc0275a46c
aldrinn <48447138+aldringordon@users.noreply.github.com>
2022-11-27 17:37:22 +0800

    feat: links and button styling

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React from "react";
 import { Cursor, useTypewriter } from "react-simple-typewriter";
 import BackgroundCircles from "./BackgroundCircles";
@@ -17,15 +18,40 @@ function Hero({}: Props) {
   return (
     <div className="flex flex-col items-center justify-center h-screen space-y-8 overflow-hidden text-center">
       <BackgroundCircles />
+      {/* eslint-disable-next-line @next/next/no-img-element */}
       <img
         className="relative object-cover w-32 h-32 mx-auto rounded-full"
         src="https://aldringordon.com/assets/Aldrin.jpg"
         alt=""
       />
-      <h1>
-        <span>{text}</span>
-        <Cursor cursorColor="#F8FFAE" />
-      </h1>
+      <div>
+        <h2 className="pb-2 text-sm text-gray-400 uppercase tracking-[15px]">
+          Software Engineer
+        </h2>
+
+        <h1 className="px-10 text-5xl font-semibold lg:text-6xl">
+          <span className="mr-3">{text}</span>
+          <Cursor cursorColor="#F8FFAE" />
+        </h1>
+
+        <div className="pt-5">
+          <Link href="#about">
+            <button className="heroButton">About</button>
+          </Link>
+
+          <Link href="#experience">
+            <button className="heroButton">Experience</button>
+          </Link>
+
+          <Link href="#skills">
+            <button className="heroButton">Skills</button>
+          </Link>
+
+          <Link href="#projects">
+            <button className="heroButton">Projects</button>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
